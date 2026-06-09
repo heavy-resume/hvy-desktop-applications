90651b2cf4e9aaf4f3047c8608a8e22c29f26f64
James Hutchison <JamesGHutchison@gmail.com>
2026-06-09 11:50:52 -0700

    Show version in about correctly

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,5 +1,7 @@
 /// <reference types="vite/client" />
 
+declare const __APP_VERSION__: string;
+
 declare module 'virtual:hvy-built-in-plugins' {
   export const builtInPluginIds: string[];
   export const builtInPlugins: any[];
